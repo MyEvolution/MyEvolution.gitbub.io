b5bd509d8bd65f89b2563d93afad0b3f32c3ab4a
MyEvolution <wlsdzyzl@gmail.com>
2019-04-23 22:19:09 +0800

    Site updated: 2019-04-23 22:19:08

---
+++ code/histogram.py
@@ -0,0 +1,57 @@
+# python histogram equalization
+import math
+import numpy as np
+from PIL import Image
+from pylab import *
+
+def equalization(im):
+    obj = im.flatten()
+    sta = [0 for i in range(256)]
+    for i in obj:
+        sta[i] = sta[i]+1
+    sta = [i /len(obj) for i in sta ]
+
+    updated = []
+    integral=0.0
+    for i in sta:
+        integral += i
+        updated.append(int(integral * 255))
+    nim = np.array(im)
+    for i in range(len(im)):
+        for j in range(len(im[i])):
+            nim[i][j] = updated[im[i][j]]
+    return nim,updated
+def specification(im,im_s):
+    nim,updated1 = equalization(im)
+
+    nim_s,updated2 = equalization(im_s)
+    updated = [-1 for i in range(256)]
+    for i in range(256):
+        if updated[updated2[i]]!=-1:
+            updated[updated2[i]] = i
+    final = np.array(im)
+    for i in range(len(im)):
+        for j in range(len(im[i])):
+            if updated[im[i][j]]!= -1:
+                final[i][j] = updated[im[i][j]]
+    return final
+
+
+
+im = np.array(Image.open("./object.jpg").convert("L"))
+nim = equalization(im)[0]
+im_s = np.array(Image.open("./image.jpg").convert("L"))
+final = specification(im,im_s)
+
+Image.fromarray(im).show()
+#Image.fromarray(nim).show()
+#Image.fromarray(im_s).show()
+Image.fromarray(final).show()
+figure()
+hist(im.flatten(),256)
+hist(im_s.flatten(),256)
+hist(final.flatten(),256)
+show()
+# python histogram specification
+
+
